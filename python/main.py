27fed2d6b18bee56f052f15c62808626aaed32b7
Roy Ready <roy.r.phys@gmail.com>
2016-08-27 14:01:02 -0400

    finished functions 'Print' and 'BoxPrint'

--- python/main.py
+++ python/main.py
@@ -2,7 +2,9 @@ from printer import *
 
 def main():
     PrintAdventure()
-
+    BoxPrint("these are some words that will wrap over the line when the length of" 
+		" the words surpasses the width of the command line ")
+    
 if __name__ == '__main__':
     main()
 
